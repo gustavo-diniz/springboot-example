4e17e6ff38f98608ffccf670627e1b8645839044
gustavo diniz <gustavodiniz@MacBook-Pro-de-gustavo.local>
2018-07-26 00:44:35 -0300

    incluido exemplo com properties interno

--- src/main/java/br/com/gustavodiniz/application/Application.java
+++ src/main/java/br/com/gustavodiniz/application/Application.java
@@ -1,14 +1,22 @@
 package br.com.gustavodiniz.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("br.com")
 public class Application {
 
-	
+	@Autowired
+	private ServerProperties serverProperties;
+
+	public void run(String... args) throws Exception {
+		System.out.println(serverProperties);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/br/com/gustavodiniz/controller/HelloController.java
+++ src/main/java/br/com/gustavodiniz/controller/HelloController.java
@@ -1,21 +1,15 @@
 package br.com.gustavodiniz.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.gustavodiniz.facade.PosicaoFacade;
 import br.com.gustavodiniz.vo.Pessoa;
-import br.com.logicx.to.FiltroObjetosTO;
-import br.com.logicx.vo.FiltroObjetosVO;
-import br.com.logicx.vo.PosicaoSensorVOList;
 
 @RestController
 public class HelloController {
 
-	@Autowired
-	private PosicaoFacade posicaoFacade;
+
 
 	@RequestMapping(value = "/pessoa", method = RequestMethod.GET, headers = "Accept=application/json")
 	public Pessoa consultaPessoa() {
@@ -39,11 +33,6 @@ public class HelloController {
 		return p;
 	}
 
-	@RequestMapping(value = "/atual", method = RequestMethod.GET, headers = "Accept=application/json")
-	public PosicaoSensorVOList exemplo() {
-		FiltroObjetosTO to = posicaoFacade.validaFiltroObjetos(new FiltroObjetosVO());
-		PosicaoSensorVOList vo = posicaoFacade.buscaPosicaoSensor(to);
-		return vo;
-	}
+	
 
 }
